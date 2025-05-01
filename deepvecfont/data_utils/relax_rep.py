3377fd4306a1e499b883698d668a1f4bec23815c
Simon Cozens <simon@simon-cozens.org>
2025-05-01 11:22:41 +0100

    Fold this into build_dataset

--- deepvecfont/data_utils/relax_rep.py
+++ deepvecfont/data_utils/relax_rep.py
@@ -1,35 +1,32 @@
-import argparse
-import multiprocessing as mp
-import os
-
 import numpy as np
-import tqdm
+
+from deepvecfont.data_utils.svg_utils import MAX_SEQ_LEN
+
+# Denumericalize works fine with numpy arrays and torch tensors, numericalize only works with torch...
+from deepvecfont.models.transformers import denumericalize
 
 
-def numericalize(cmd, n=64):
+# ...so we need our own numericalize function
+def numericalize(cmd, n=128):
     """NOTE: shall only be called after normalization"""
-    cmd = ((cmd) / 30 * n).round().clip(min=0, max=n - 1).astype(int)
+    # assert np.max(cmd.origin) <= 1.0 and np.min(cmd.origin) >= -1.0
+    cmd = (cmd / 30 * n).round().clip(min=0, max=n - 1).astype(int)
     return cmd
 
 
-def denumericalize(cmd, n=64):
-    cmd = cmd / n * 30
-    return cmd
-
-
-def cal_aux_bezier_pts(font_seq, opts):
+def cal_aux_bezier_pts(font_seq, n_chars):
     """
     calculate aux pts along bezier curves
     """
     pts_aux_all = []
 
-    for j in range(opts.n_chars):
-        char_seq = font_seq[j]  # shape: opts.max_len ,12
+    for j in range(n_chars):
+        char_seq = font_seq[j]  # shape: MAX_SEQ_LEN ,12
         pts_aux_char = []
-        for k in range(opts.max_len):
+        for k in range(MAX_SEQ_LEN):
             stroke_seq = char_seq[k]
             stroke_cmd = np.argmax(stroke_seq[:4], -1)
-            stroke_seq[4:] = denumericalize(numericalize(stroke_seq[4:]))
+            stroke_seq[4:] = denumericalize(numericalize(stroke_seq[4:], n=64), n=64)
             p0, p1, p2, p3 = (
                 stroke_seq[4:6],
                 stroke_seq[6:8],
@@ -72,84 +69,30 @@ def cal_aux_bezier_pts(font_seq, opts):
     return pts_aux_all
 
 
-def relax_rep(opts):
-    """
-    relaxing the sequence representation, details are shown in paper
-    """
-    data_path = os.path.join(opts.output_path, opts.language, opts.split)
-    font_dirs = os.listdir(data_path)
-    font_dirs.sort()
-    num_fonts = len(font_dirs)
-    print(f"Number {opts.split} fonts before processing", num_fonts)
-    for i in tqdm.tqdm(range(num_fonts)):
+def relax_a_character(char_len, char_cmds, char_args):
+    new_args = []
+    pre_arg = None
+    for k in range(char_len):
+        cur_cls = np.argmax(char_cmds[k], -1)
+        cur_arg = char_args[k]
+        if k > 0:
+            pre_arg = char_args[k - 1]
+        if cur_cls == 1:  # when k == 0, cur_cls == 1
+            cur_arg = np.concatenate(
+                (np.array([cur_arg[-2], cur_arg[-1]]), cur_arg), -1
+            )
+        else:
+            cur_arg = np.concatenate(
+                (np.array([pre_arg[-2], pre_arg[-1]]), cur_arg), -1
+            )
+        new_args.append(cur_arg)
 
-        font_dir = os.path.join(data_path, font_dirs[i])
-        font_seq = np.load(os.path.join(font_dir, "sequence.npy")).reshape(
-            opts.n_chars, opts.max_len, -1
-        )
-        font_len = np.load(os.path.join(font_dir, "seq_len.npy")).reshape(-1)
-        cmd = font_seq[:, :, :4]
-        args = font_seq[:, :, 4:]
+    while (len(new_args)) < MAX_SEQ_LEN + 1:
+        new_args.append(np.array([0, 0, 0, 0, 0, 0, 0, 0]))
 
-        ret = []
-        for j in range(opts.n_chars):
-
-            char_cmds = cmd[j]
-            char_args = args[j]
-            char_len = font_len[j]
-            new_args = []
-            for k in range(char_len):
-                cur_cls = np.argmax(char_cmds[k], -1)
-                cur_arg = char_args[k]
-                if k - 1 > -1:
-                    pre_arg = char_args[k - 1]
-                if cur_cls == 1:  # when k == 0, cur_cls == 1
-                    cur_arg = np.concatenate(
-                        (np.array([cur_arg[-2], cur_arg[-1]]), cur_arg), -1
-                    )
-                else:
-                    cur_arg = np.concatenate(
-                        (np.array([pre_arg[-2], pre_arg[-1]]), cur_arg), -1
-                    )
-                new_args.append(cur_arg)
-
-            while (len(new_args)) < opts.max_len:
-                new_args.append(np.array([0, 0, 0, 0, 0, 0, 0, 0]))
-
-            new_args = np.array(new_args)
-            new_seq = np.concatenate((char_cmds, new_args), -1)
-            ret.append(new_seq)
-        ret = np.array(ret)
-        # write relaxed version of sequence.npy
-        np.save(
-            os.path.join(font_dir, "sequence_relaxed.npy"),
-            ret.reshape(opts.n_chars, -1),
-        )
-
-        pts_aux = cal_aux_bezier_pts(ret, opts)
-        np.save(os.path.join(font_dir, "pts_aux.npy"), pts_aux)
+    new_args = np.array(new_args)
+    new_seq = np.concatenate((char_cmds, new_args), -1)
+    return new_seq
 
 
-def main():
-    parser = argparse.ArgumentParser(description="relax representation")
-    parser.add_argument("--language", type=str, default="eng", choices=["eng", "chn"])
-    parser.add_argument(
-        "--output_path",
-        type=str,
-        default="../data/vecfont_dataset_/",
-        help="Path to write the database to",
-    )
-    parser.add_argument(
-        "--max_len",
-        type=int,
-        default=51,
-        help="by default, 51 for english and 71 for chinese",
-    )
-    parser.add_argument("--n_chars", type=int, default=51)
-    parser.add_argument("--split", type=str, default="train")
-    opts = parser.parse_args()
-    relax_rep(opts)
-
-
-if __name__ == "__main__":
-    main()
+# No need for this step any more, it's part of write_data_to_dirs
