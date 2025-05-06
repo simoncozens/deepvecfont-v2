5ed6c774132093d92d0901db2704056933567b66
Simon Cozens <simon@simon-cozens.org>
2025-05-06 19:12:50 +0100

    More unused

--- deepvecfont/data_utils/make_dataset.py
+++ deepvecfont/data_utils/make_dataset.py
@@ -20,7 +20,7 @@ from deepvecfont.options import add_language_arg, get_charset
 
 generator1 = torch.Generator().manual_seed(42)
 
-BANNED = ["noto", "bitcount", "adobeblank"]
+BANNED = ["noto", "bitcount", "adobeblank", "nabla", "handjet"]
 
 NUM_AXIS_INSTANCES = 5  # Number of instances to generate for each axis
 
@@ -53,7 +53,7 @@ def create_db(opts, output_path, log_path):
     print(f"Number {opts.split} fonts before processing", num_fonts)
 
     for i, (font_path, location) in tqdm.tqdm(
-        enumerate(fonts_locations), total=num_fonts
+        enumerate(fonts_locations), total=num_fonts, miniters=1, smoothing=0.1
     ):
         font, upem = make_hb_font(font_path)
         if location:
@@ -163,48 +163,6 @@ def load_font_glyphs(charset, font_path, font, upem, missing_ok=False):
     return [svg_utils.create_example(pathunibfp, upem) for pathunibfp in good_paths]
 
 
-def cal_mean_stddev(opts, output_path):
-    print("Calculating all glyphs' mean stddev ....")
-    charset = get_charset(opts)
-    font_paths = []
-    for _root, dirs, _files in os.walk(output_path):
-        for dir_name in dirs:
-            font_paths.append(os.path.join(output_path, dir_name))
-    font_paths.sort()
-    num_fonts = len(font_paths)
-    num_chars = len(charset)
-    main_stddev_accum = svg_utils.MeanStddev()
-
-    cur_sum_count = main_stddev_accum.create_accumulator()
-    for i in tqdm.tqdm(range(0, num_fonts)):
-        cur_font_path = font_paths[i]
-        seqlens = np.load(os.path.join(cur_font_path, "seq_len.npy")).tolist()
-        sequences = np.load(os.path.join(cur_font_path, "sequence.npy")).tolist()
-        for charid in range(num_chars):
-            cur_font_char = {}
-            cur_font_char["seq_len"] = seqlens[charid]
-            cur_font_char["sequence"] = sequences[charid]
-            cur_sum_count = main_stddev_accum.add_input(cur_sum_count, cur_font_char)
-
-    output = main_stddev_accum.extract_output(cur_sum_count)
-    mean = output["mean"]
-    stdev = output["stddev"]
-    # mean = np.concatenate((np.zeros([4]), mean[4:]), axis=0)
-    # stdev = np.concatenate((np.ones([4]), stdev[4:]), axis=0)
-    # finally, save the mean and stddev files
-    output_path_ = os.path.join(opts.output_path, opts.language)
-    np.save(os.path.join(output_path_, "mean"), mean)
-    np.save(os.path.join(output_path_, "stdev"), stdev)
-
-    # rename npy to npz, don't mind about it, just some legacy issue
-    os.rename(
-        os.path.join(output_path_, "mean.npy"), os.path.join(output_path_, "mean.npz")
-    )
-    os.rename(
-        os.path.join(output_path_, "stdev.npy"), os.path.join(output_path_, "stdev.npz")
-    )
-
-
 def augment_variables(all_font_paths):
     for path in all_font_paths:
         ttfont = TTFont(path)
@@ -238,13 +196,6 @@ def main():
         "--img_size", type=int, default=64, help="the height and width of glyph images"
     )
     parser.add_argument("--split", type=str, default="train")
-    parser.add_argument(
-        "--phase",
-        type=int,
-        default=0,
-        choices=[0, 1, 2],
-        help="0 all, 1 create db, 2 cal stddev",
-    )
 
     opts = parser.parse_args()
 
@@ -257,11 +208,7 @@ def main():
     if not os.path.exists(log_path):
         os.makedirs(log_path)
 
-    if opts.phase <= 1:
-        create_db(opts, output_path, log_path)
-
-    if opts.phase <= 2 and opts.split == "train":
-        cal_mean_stddev(opts, output_path)
+    create_db(opts, output_path, log_path)
 
 
 if __name__ == "__main__":
